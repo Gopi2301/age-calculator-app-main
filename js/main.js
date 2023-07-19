8e6acd6d417d7c2e2638e07c51329389f4c244b5
Gopi2301 <gopinaths2301@gmail.com>
2023-07-19 16:16:37 +0530

    calculation logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-const date = new Date();
-const presentYear = date.getFullYear();
-const presentMonth = date.getMonth() + 1;
-const presentDay = date.getDate();
+// const { current } = require("@reduxjs/toolkit");
+
+const currentDate = new Date();
 const Button = document.getElementById('submit');
 const inputDay = document.getElementById('day');
-const inputMonth = document.getElementById('month')
-const inputYear = document.getElementById('year')
+const inputMonth = document.getElementById('month');
+const inputYear = document.getElementById('year');
+const outputDay = document.querySelector('.card__output--day');
+const outputMonth = document.querySelector('.card__output--month');
+const outputYear = document.querySelector('.card__output--year');
 
-const inputDate = [];
 
 
 Button.addEventListener('click', (event) => {
     event.preventDefault();
-    inputDate.push((inputDay.value), (inputMonth.value), (inputYear.value))
-})
+    let birthday = new Date(0, 0, 0);
+    birthday = new Date(inputYear.value, inputMonth.value - 1, inputDay.value);
+    if (birthday < currentDate) {
+        logic(birthday)
+    } else {
+        console.log('error');
+    }
+
+});
+
+function logic(inputDate) {
+    var ageYears = currentDate.getFullYear() - inputDate.getFullYear();
+
+    if (
+        currentDate.getMonth() < inputDate.getMonth() ||
+        (currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() < inputDate.getDate())
+    ) {
+        ageYears--; // Adjust age if birth month and day haven't passed yet
+    }
+
+    var ageMonths = currentDate.getMonth() - inputDate.getMonth();
+    if (currentDate.getDate() < inputDate.getDate()) {
+        ageMonths--; // Adjust months if birth day hasn't passed yet
+    }
+    if (ageMonths < 0) {
+        ageMonths += 12; // Handle negative months by adding 12
+    }
+    var ageDays = currentDate.getDate() - inputDate.getDate();
+    if (ageDays < 0) {
+        // Get the last day of the previous month
+        var lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+        ageDays = lastMonthDate.getDate() - inputDate.getDate() + currentDate.getDate();
+    }
+    outputDay.innerHTML = ageDays;
+    outputMonth.innerHTML = ageMonths;
+    outputYear.innerHTML = ageYears;
+    console.log("Age: " + ageYears + " years, " + ageMonths + " months, " + ageDays + " days.");
+}
+
+
